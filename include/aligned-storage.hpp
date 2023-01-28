85b857912fb237bc40afd529bf42cc9fa113cec2
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-01-29 01:04:21 +0500

    Add impl. of std::aligned_storage

---
+++ include/aligned-storage.hpp
@@ -0,0 +1,25 @@
+#ifndef __ALIGNED_STORAGE__
+#define __ALIGNED_STORAGE__
+
+template <typename T, size_t Size>
+class AlignedStorage
+{
+private:
+    uint8_t storage[sizeof(T) * Size + alignof(T)];
+    void *begin = storage;
+
+public:
+    AlignedStorage()
+    {
+        uintptr_t padding = (uintptr_t)((void *)(storage)) % alignof(T);
+        if (padding != 0)
+            begin = storage + alignof(T) - padding;
+    }
+
+    T *get(const size_t index)
+    {
+        return std::static_cast<T*>(begin) + index;
+    }
+};
+
+#endif
