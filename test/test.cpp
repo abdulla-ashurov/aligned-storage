7811cff9d0d4401a93bdf711ddfd88656859c699
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-01-29 01:12:19 +0500

    Update unit tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,19 @@
+#include <string>
 #include "catch_amalgamated.hpp"
 #include "../include/aligned-storage.hpp"
 
 TEST_CASE("test AlignedStorage class")
 {
+    const size_t size = 4;
+    const size_t default_alignment = 1;
+    
     SECTION("test AlignedStorage class for uint32_t type")
     {
-        const size_t size = 4;
-        const size_t default_alignment = 1;
-
-        #pragma pack(default_alignment)
+        #pragma pack(1)
         struct TestType
         {
             char symbol;
-            AlignedStorage<uint32_t, 4> storage;
+            AlignedStorage<uint32_t, size> storage;
         };
 
         TestType test;
@@ -24,14 +25,11 @@ TEST_CASE("test AlignedStorage class")
 
     SECTION("test AlignedStorage class for uint64_t type")
     {
-        const size_t size = 4;
-        const size_t default_alignment = 1;
-
-        #pragma pack(default_alignment)
+        #pragma pack(1)
         struct TestType
         {
             char symbol;
-            AlignedStorage<uint64_t, 4> storage;
+            AlignedStorage<uint64_t, size> storage;
         };
         
         TestType test;
@@ -39,5 +37,21 @@ TEST_CASE("test AlignedStorage class")
         REQUIRE(alignof(TestType) == default_alignment);
         for (size_t i = 0; i < size; i++)
             REQUIRE(uintptr_t((void *)(test.storage.get(i))) % alignof(uint64_t) == 0);
+    }
+
+    SECTION("test AlignedStorage class for std::string type")
+    {
+        #pragma pack(1)
+        struct TestType
+        {
+            char symbol;
+            AlignedStorage<std::string, size> storage;
+        };
+        
+        TestType test;
+
+        REQUIRE(alignof(TestType) == default_alignment);
+        for (size_t i = 0; i < size; i++)
+            REQUIRE(uintptr_t((void *)(test.storage.get(i))) % alignof(std::string) == 0);
     }
 }
